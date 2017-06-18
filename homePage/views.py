48990b4d5a4d131c7c3f1f980f7d01bd07614618
volodymyrhusak <vovatrap@gmail.com>
2017-06-18 19:43:52 +0300

    2

--- homePage/views.py
+++ homePage/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from blog.models import PostModel
 from django.db.models import Count
+from django.conf import settings
 
 # Create your views here.
 
 def homePage(request):
     # posts = PostModel.objects.order_by('-createTime')[:6]
     posts = PostModel.objects.annotate(Count('postcommentmodel')).order_by('-createTime')[:6]
-    context = {'posts':posts}
+    context = {'MY_DATE_FORMAT': settings.MY_DATE_FORMAT,
+    		'posts':posts}
     return render(request, 'homePage.html', context)
