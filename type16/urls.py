4751b5b5ebf4874a505ca771a9ba55d4700534d4
volodymyrhusak <vovatrap@gmail.com>
2017-06-19 11:26:00 +0300

    32

--- type16/urls.py
+++ type16/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
 
 ]
 if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
