ef084afec0aa59b97c2d545ae02379aa6625f5c8
volodymyrhusak <vovatrap@gmail.com>
2017-07-12 12:31:34 +0300

    +mysql DB

--- type16/settings.py
+++ type16/settings.py
@@ -93,11 +93,22 @@ WSGI_APPLICATION = 'type16.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
+#         'CONN_MAX_AGE': 500
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
-        'CONN_MAX_AGE': 500
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': ' blog16types',
+        'USER': ' blog16types',
+        'PASSWORD': 'HLPeYXZsBwt55Lhw',
+        'HOST': '188.226.157.20/phpmyadmin',
+        'PORT': '',
     }
 }
 
